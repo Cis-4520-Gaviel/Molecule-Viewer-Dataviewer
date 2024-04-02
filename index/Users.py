83b835ae5f587e131dd8200cc20d0802286d6ec2
DanWang725 <dw.smaug@gmail.com>
2024-04-01 22:44:00 -0400

    add more methods to users

--- index/Users.py
+++ index/Users.py
@@ -1,19 +1,31 @@
 import os
 from pymcl import Fr, g1, g2
 from abc import ABC, abstractmethod
+from Trapdoor import generateTrapdoor
+
 class User(ABC):
     @abstractmethod
     def _setup(self):
         pass
+
+    def __init__(self, queryMultiplexer, dataHost):
+        self.QM = queryMultiplexer
+        self.DH = dataHost
 
 class Writer(User):
     def _setup(self):
         secretKey = Fr.random()
         return secretKey
 
-    def __init__(self):
+    def __init__(self, queryMultiplexer, dataHost):
         self._secretKey = self._setup()
+        super(queryMultiplexer, dataHost)
 
+    def delegate(self, readerPublicKey: Fr):
+        auth = readerPublicKey * self._secretKey
+        #send to QM
+
+    
     #database
 
 class Reader(User):
@@ -23,11 +35,16 @@ class Reader(User):
         kR = os.urandom(8)
         return publicKey, privateKey, kR
 
-    def __init__(self):
+    def __init__(self, queryMultiplexer, dataHost):
         publicKey, privateKey, kR = self._setup()
         self._publicKey = publicKey
         self._privateKey = privateKey
         self._kR = kR
+        super(queryMultiplexer, dataHost)
 
     def getPublicKey(self):
         return self._publicKey
+    
+    def trapdoor(self, sqlStatement):
+        yield Exception("need to implement this")
+        generateTrapdoor(sqlStatement, 10) # nuh uh
