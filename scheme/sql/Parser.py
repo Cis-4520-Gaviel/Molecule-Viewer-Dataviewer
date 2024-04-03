8dc3e2d270f6c2979529f96bd72873782bd1dd42
DanWang725 <dw.smaug@gmail.com>
2024-04-03 16:28:33 -0400

    move keyword extraction method

--- scheme/sql/Parser.py
+++ scheme/sql/Parser.py
@@ -30,6 +30,40 @@ def getNonWhitespaceTokens(tokens):
     return arr
 
 
+def getSelectKeywords(sql):
+    """
+    This basically extracts the search keywords out of an sql statement. 
+    DO NOT supply statements which do not contain statements. There is no error handling currently
+    """
+
+    # print(sql)
+    output = sqlparse.format(sql, reindent=True)
+    # print(output)
+    tokens = sqlparse.parse(sql)
+    # print('Tokens:',tokens[0].tokens)
+    for token in tokens[0].tokens:
+        if(isinstance(token, sqlparse.sql.Where)):
+            temp = ""
+            token2 = token
+            i = 1
+            while(token2.token_next(i, skip_ws=False)[1] is not None):
+                curToken = token2.token_next(i, skip_ws=False)[1]
+                # print(curToken, curToken.ttype, curToken.value, curToken.__class__)
+                # if isinstance(curToken, sqlparse.sql.Identifier) or isinstance(curToken, sqlparse.sql.Token):
+                if str(curToken) != " " and str(curToken) != ";":
+                    temp += str(curToken)
+                        
+                i = i + 1
+            # print(token.token_next(1)[1])
+            # print("output: ",temp)
+            # print("comparisons:", temp.split("AND"))
+            return temp.split("AND")
+            # nextToken = token.token_next(1)
+            # print('Occurrances',nextToken)
+            # print(nextToken[1])
+            # print("JALSDJFLKAJD")
+        # print("\nstuff", token)
+
 def parseCreateStatement(sqlStatement):
     # print(sqlStatement)
     sections = sqlparse.format(sqlStatement, reindent=True, keyword_case='upper')
--- scheme/users/Trapdoor.py
+++ scheme/users/Trapdoor.py
@@ -2,40 +2,7 @@ import sqlparse
 from CryptoUtils import AESSIVEncryptNonce, phiFunction, get_xor
 from pymcl import g1, pairing
 import os
-
-def ExtractKeywords(sql):
-    """
-    This basically extracts the search keywords out of an sql statement. 
-    DO NOT supply statements which do not contain statements. There is no error handling currently
-    """
-
-    # print(sql)
-    output = sqlparse.format(sql, reindent=True)
-    # print(output)
-    tokens = sqlparse.parse(sql)
-    # print('Tokens:',tokens[0].tokens)
-    for token in tokens[0].tokens:
-        if(isinstance(token, sqlparse.sql.Where)):
-            temp = ""
-            token2 = token
-            i = 1
-            while(token2.token_next(i, skip_ws=False)[1] is not None):
-                curToken = token2.token_next(i, skip_ws=False)[1]
-                # print(curToken, curToken.ttype, curToken.value, curToken.__class__)
-                # if isinstance(curToken, sqlparse.sql.Identifier) or isinstance(curToken, sqlparse.sql.Token):
-                if str(curToken) != " " and str(curToken) != ";":
-                    temp += str(curToken)
-                        
-                i = i + 1
-            # print(token.token_next(1)[1])
-            # print("output: ",temp)
-            # print("comparisons:", temp.split("AND"))
-            return temp.split("AND")
-            # nextToken = token.token_next(1)
-            # print('Occurrances',nextToken)
-            # print(nextToken[1])
-            # print("JALSDJFLKAJD")
-        # print("\nstuff", token)
+from Parser import getSelectKeywords
 
 def generateTrapdoor(sql, K):
     """
@@ -61,7 +28,7 @@ def generateTrapdoorBLS12381(sql, privKey):
     """
     # (Kpsi, Kpi, Kphi) = K # realistically we would be generating our own curve, but for this case we use the predefined ones
     print('input SQL:', sql)
-    keywords = ExtractKeywords(sql)
+    keywords = getSelectKeywords(sql)
     # print('extract keywords:', keywords)
     # AESSIVEncryptNonce(K, keywords[0])
     trapdoors = []
